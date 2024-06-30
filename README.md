019f0522e50613b42be349e6ddd32514ab479969
Faheem2407 <faheem24072000@gmail.com>
2024-06-30 15:46:44 +0600

    Version: 0.0.1 of Personal Portfolio
    - Home,About and Contact page is created and working successfully
    
    - Some code duplications are present we will reduce this in next version

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # PHP-Portfolio-Project
 ---
 Here I will make a portfolio templete without any framework but use MVC pattern.
+
+There will be three page:  
+    <ul>
+	<li>Home</li>
+    <li>About</li>
+    <li>Contact us</li>
+	</ul>
---
+++ about.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+	<title>Personal Portfolio</title>
+	<link rel="stylesheet" href="public/bootstrap.css">
+</head>
+<body>
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+		<div class="container-fluid">
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+			  <span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarNav">
+			  <ul class="navbar-nav">
+				<li class="nav-item">
+				  <a class="nav-link active" aria-current="page" href="/">Home</a>
+				</li>
+				<li class="nav-item">
+				  <a class="nav-link" href="/about.php">About</a>
+				</li>
+				<li class="nav-item">
+				  <a class="nav-link" href="/contact.php">Contact Us</a>
+				</li>
+			  </ul>
+			</div>
+		</div>
+	</nav>
+
+	<div class="container">
+		Welcome to About Page
+	</div>
+
+</body>
+</html>
